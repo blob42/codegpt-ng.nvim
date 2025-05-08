5ab74959d33b6fa4e6e1230a97922c9da39cf809
blob42 <contact@blob42.xyz>
2025-05-08 17:13:15 +0200

    ui and connectivitiy params
    todo: connection proxy
    Signed-off-by: blob42 <contact@blob42.xyz>

--- lua/codegpt/api.lua
+++ lua/codegpt/api.lua
@@ -1,4 +1,4 @@
-local curl = require("plenary.curl")
+local Config = require("codegpt.config")
 
 local Api = {}
 
@@ -8,33 +8,32 @@ local status_index = 0
 local progress_bar_dots = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
 
 function Api.get_status(...)
-    if CODEGPT_CALLBACK_COUNTER > 0 then
-        status_index = status_index + 1
-        if status_index > #progress_bar_dots then
-            status_index = 1
-        end
-        return progress_bar_dots[status_index]
-    else
-        return ""
-    end
+	if CODEGPT_CALLBACK_COUNTER > 0 then
+		status_index = status_index + 1
+		if status_index > #progress_bar_dots then
+			status_index = 1
+		end
+		return progress_bar_dots[status_index]
+	else
+		return ""
+	end
 end
 
 function Api.run_started_hook()
-    if vim.g["codegpt_hooks"]["request_started"] ~= nil then
-        vim.g["codegpt_hooks"]["request_started"]()
-    end
+	if Config.opts.hooks.request_started ~= nil then
+		Config.opts.hooks.request_started()
+	end
 
-    CODEGPT_CALLBACK_COUNTER = CODEGPT_CALLBACK_COUNTER + 1
+	CODEGPT_CALLBACK_COUNTER = CODEGPT_CALLBACK_COUNTER + 1
 end
 
 function Api.run_finished_hook()
-    CODEGPT_CALLBACK_COUNTER = CODEGPT_CALLBACK_COUNTER - 1
-    if CODEGPT_CALLBACK_COUNTER <= 0 then
-        if vim.g["codegpt_hooks"]["request_finished"] ~= nil then
-            vim.g["codegpt_hooks"]["request_finished"]()
-        end
-    end
+	CODEGPT_CALLBACK_COUNTER = CODEGPT_CALLBACK_COUNTER - 1
+	if CODEGPT_CALLBACK_COUNTER <= 0 then
+		if Config.opts.hooks.request_finished ~= nil then
+			Config.opts.hooks.request_finished()
+		end
+	end
 end
-
 
 return Api
--- lua/codegpt/providers.lua
+++ lua/codegpt/providers.lua
@@ -3,24 +3,25 @@ local AzureProvider = require("codegpt.providers.azure")
 local AnthropicProvider = require("codegpt.providers.anthropic")
 local OllaMaProvider = require("codegpt.providers.ollama")
 local GroqProvider = require("codegpt.providers.groq")
+local Config = require("codegpt.config")
 
 Providers = {}
 
 function Providers.get_provider()
-    local provider = vim.fn.tolower(vim.g["codegpt_api_provider"])
-    if provider == "openai" then
-        return OpenAIProvider
-    elseif provider == "azure" then
-        return AzureProvider
-    elseif provider == "anthropic" then
-        return AnthropicProvider
-    elseif provider == "ollama" then
-        return OllaMaProvider
-    elseif provider == "groq" then
-        return GroqProvider
-    else
-        error("Provider not found: " .. provider)
-    end
+	local provider = vim.fn.tolower(Config.opts.connection.api_provider)
+	if provider == "openai" then
+		return OpenAIProvider
+	elseif provider == "azure" then
+		return AzureProvider
+	elseif provider == "anthropic" then
+		return AnthropicProvider
+	elseif provider == "ollama" then
+		return OllaMaProvider
+	elseif provider == "groq" then
+		return GroqProvider
+	else
+		error("Provider not found: " .. provider)
+	end
 end
 
 return Providers
