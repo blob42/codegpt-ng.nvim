06da9fbf60131f75683306f3946da66f941b2adf
blob42 <contact@blob42.xyz>
2025-05-11 02:59:50 +0200

    stream: fix missing tail of stream
    Signed-off-by: blob42 <contact@blob42.xyz>

--- lua/codegpt/ui.lua
+++ lua/codegpt/ui.lua
@@ -1,6 +1,6 @@
 local Popup = require("nui.popup")
 local Split = require("nui.split")
-local Config = require("codegpt.config")
+local config = require("codegpt.config")
 local event = require("nui.utils.autocmd").event
 
 local M = {}
@@ -14,7 +14,7 @@ local function create_horizontal()
 		split = Split({
 			relative = "editor",
 			position = "bottom",
-			size = Config.opts.ui.horizontal_popup_size,
+			size = config.opts.ui.horizontal_popup_size,
 		})
 	end
 
@@ -26,7 +26,7 @@ local function create_vertical()
 		split = Split({
 			relative = "editor",
 			position = "right",
-			size = Config.opts.ui.vertical_popup_size,
+			size = config.opts.ui.vertical_popup_size,
 		})
 	end
 
@@ -35,7 +35,7 @@ end
 
 local function create_floating()
 	if not popup then
-		local window_options = Config.opts.ui.popup_window_options
+		local window_options = config.opts.ui.popup_window_options
 		if window_options == nil then
 			window_options = {}
 		end
@@ -43,7 +43,7 @@ local function create_floating()
 		local popupOpts = {
 			enter = true,
 			focusable = true,
-			border = Config.opts.ui.popup_border,
+			border = config.opts.ui.popup_border,
 			position = "50%",
 			size = {
 				width = "80%",
@@ -58,13 +58,13 @@ local function create_floating()
 		popup = Popup(popupOpts)
 	end
 
-	popup:update_layout(Config.opts.ui.popup_options)
+	popup:update_layout(config.opts.ui.popup_options)
 
 	return popup
 end
 
 local function create_window()
-	local popup_type = Config.popup_override or Config.opts.ui.popup_type
+	local popup_type = config.popup_override or config.opts.ui.popup_type
 	local ui_elem = nil
 	if popup_type == "horizontal" then
 		ui_elem = create_horizontal()
@@ -82,7 +82,7 @@ function M.popup(lines, filetype, bufnr, start_row, start_col, end_row, end_col)
 	-- mount/open the component
 	ui_elem:mount()
 
-	if not (Config.persistent_override or Config.opts.ui.persistent) then
+	if not (config.persistent_override or config.opts.ui.persistent) then
 		-- unmount component when cursor leaves buffer
 		ui_elem:on(event.BufLeave, function()
 			ui_elem:unmount()
@@ -90,7 +90,7 @@ function M.popup(lines, filetype, bufnr, start_row, start_col, end_row, end_col)
 	end
 
 	-- unmount component when key 'q'
-	ui_elem:map("n", Config.opts.ui.commands.quit, function()
+	ui_elem:map("n", config.opts.ui.actions.quit, function()
 		ui_elem:unmount()
 	end, { noremap = true, silent = true })
 
@@ -99,19 +99,19 @@ function M.popup(lines, filetype, bufnr, start_row, start_col, end_row, end_col)
 	vim.api.nvim_buf_set_lines(ui_elem.bufnr, 0, 1, false, lines)
 
 	-- replace lines when ctrl-o pressed
-	ui_elem:map("n", Config.opts.ui.commands.use_as_output, function()
+	ui_elem:map("n", config.opts.ui.actions.use_as_output, function()
 		vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, lines)
 		ui_elem:unmount()
 	end)
 
 	-- selecting all the content when ctrl-i is pressed
 	-- so the user can proceed with another API request
-	ui_elem:map("n", Config.opts.ui.commands.use_as_input, function()
+	ui_elem:map("n", config.opts.ui.actions.use_as_input, function()
 		vim.api.nvim_feedkeys("ggVG:Chat ", "n", false)
 	end, { noremap = false })
 
 	-- mapping custom commands
-	for _, command in ipairs(vim.g.codegpt_ui_custom_commands) do
+	for _, command in ipairs(config.opts.ui.actions.custom) do
 		ui_elem:map(command[1], command[2], command[3], command[4])
 	end
 end
@@ -123,15 +123,26 @@ local stream_ui_elem = nil
 --FIXME: this callback is called for each stream inputs so multiple calls are
 --done. Should be created once at reception of first stream
 function M.popup_stream(stream, filetype, bufnr, start_row, start_col, end_row, end_col)
-	if stream == nil then
-		stream_start = false
-		return
-	end
 	if stream_start then
 		stream_ui_elem = create_window()
 
 		-- mount/open the component
 		stream_ui_elem:mount()
+
+		if not (config.persistent_override or config.opts.ui.persistent) then
+			-- unmount component when cursor leaves buffer
+			stream_ui_elem:on(event.BufLeave, function()
+				stream_ui_elem:unmount()
+			end)
+		end
+
+		-- unmount component when key 'q'
+		stream_ui_elem:map("n", config.opts.ui.actions.quit, function()
+			stream_ui_elem:unmount()
+		end, { noremap = true, silent = true })
+
+		vim.api.nvim_set_option_value("filetype", filetype, { buf = stream_ui_elem.bufnr })
+		vim.api.nvim_set_option_value("wrap", true, { win = stream_ui_elem.winid })
 
 		stream_start = false
 	end
@@ -140,35 +151,26 @@ function M.popup_stream(stream, filetype, bufnr, start_row, start_col, end_row, 
 		error("creating stream window")
 	end
 
-	if not (Config.persistent_override or Config.opts.ui.persistent) then
-		-- unmount component when cursor leaves buffer
-		stream_ui_elem:on(event.BufLeave, function()
-			stream_ui_elem:unmount()
-		end)
-	end
-
-	-- unmount component when key 'q'
-	stream_ui_elem:map("n", Config.opts.ui.commands.quit, function()
-		stream_ui_elem:unmount()
-	end, { noremap = true, silent = true })
-
-	vim.api.nvim_set_option_value("filetype", filetype, { buf = stream_ui_elem.bufnr })
-
-	local payload = vim.fn.json_decode(stream)
-	local content = payload.message.content
 	local lines = {}
-	local content_lines = vim.split(content, "\n")
-	if #content_lines == 1 then
-		buffer = buffer .. content_lines[1]
-	elseif #content_lines > 1 then
-		if #buffer > 0 then
+	if stream == nil and #buffer > 0 then
+		table.insert(lines, buffer)
+		buffer = ""
+	else
+		local payload = vim.fn.json_decode(stream)
+		local content = payload.message.content
+		local content_lines = vim.split(content, "\n")
+		if #content_lines == 1 then
 			buffer = buffer .. content_lines[1]
-			table.insert(lines, buffer)
-			buffer = ""
-		end
+		elseif #content_lines > 1 then
+			if #buffer > 0 then
+				buffer = buffer .. content_lines[1]
+				table.insert(lines, buffer)
+				buffer = ""
+			end
 
-		for line in vim.iter(content_lines):skip(1) do
-			table.insert(lines, line)
+			for line in vim.iter(content_lines):skip(1) do
+				table.insert(lines, line)
+			end
 		end
 	end
 	if #lines > 0 then
