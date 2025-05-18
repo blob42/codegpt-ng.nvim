57c65ddfbedddcaf6fe70b2063b61aed2b7ace10
blob42 <contact@blob42.xyz>
2025-05-19 00:45:29 +0200

    chore: clean comments
    Signed-off-by: blob42 <contact@blob42.xyz>

--- lua/codegpt/config.lua
+++ lua/codegpt/config.lua
@@ -1,39 +1,6 @@
 ---@class codegpt.Config
 local M = {}
 
--- -- Read old config if it exists
--- if vim.g["codegpt_openai_api_provider"] and #vim.g["codegpt_openai_api_provider"] > 0 then
--- 	vim.g["codegpt_api_provider"] = vim.g["codegpt_openai_api_provider"]
--- end
-
--- clears visual selection after completion
--- vim.g["codegpt_clear_visual_selection"] = true
-
--- vim.g["codegpt_hooks"] = {
--- 	request_started = nil,
--- 	request_finished = nil,
--- }
-
--- Border style to use for the popup
--- vim.g["codegpt_popup_border"] = { style = "rounded" }
-
--- Wraps the text on the popup window, deprecated in favor of codegpt_popup_window_options
--- vim.g["codegpt_wrap_popup_text"] = true
-
--- vim.g["codegpt_popup_window_options"] = {}
-
--- set the filetype of a text popup is markdown
--- vim.g["codegpt_text_popup_filetype"] = "markdown"
-
--- Set the type of ui to use for the popup, options are "popup", "vertical" or "horizontal"
--- vim.g["codegpt_popup_type"] = "popup"
-
--- Set the height of the horizontal popup
--- vim.g["codegpt_horizontal_popup_size"] = "20%"
-
--- Set the width of the vertical popup
--- vim.g["codegpt_vertical_popup_size"] = "20%"
-
 ---@class codegpt.CommandOpts
 ---@field user_message_template? string
 ---@field language_instructions? table<string, string> language instruction in the form lang = instruction
@@ -106,10 +73,6 @@ local default_commands = {
 		callback_type = "text_popup",
 	},
 }
-
----
---- Refactor to idiomatic neovim plugin
----
 
 M.model_override = nil
 M.popup_override = nil
