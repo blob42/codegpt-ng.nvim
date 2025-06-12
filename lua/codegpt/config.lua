d00b18582364c12cece3f28d8853bfb4f97f2fff
blob42 <contact@blob42.xyz>
2025-06-12 13:17:45 +0200

    `doc` command prompt

--- lua/codegpt/config.lua
+++ lua/codegpt/config.lua
@@ -53,7 +53,7 @@ local default_commands = {
 		callback_type = "text_popup",
 	},
 	["doc"] = {
-		user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nWrite really good documentation using best practices for the given language. Attention paid to documenting parameters, return types, any exceptions or errors. {{language_instructions}} Only return the code snippet and nothing else.",
+		user_message_template = "I have the following {{language}} code: ```{{filetype}}\n{{text_selection}}```\nWrite really good documentation using best practices for the given language. Attention paid to documenting parameters, return types, any exceptions or errors. {{language_instructions}} Only return the documentation snippet and nothing else.",
 		language_instructions = {
 			cpp = "Use doxygen style comments for functions.",
 			java = "Use JavaDoc style comments for functions.",
